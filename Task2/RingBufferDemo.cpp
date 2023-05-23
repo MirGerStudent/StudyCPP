3871dab36b8346608b3249438f88300068aec923
Mikula GerHard <mikulagerhard00@gmail.com>
2023-05-23 16:09:33 +0600

    [*] fix

--- Task2/RingBufferDemo.cpp
+++ Task2/RingBufferDemo.cpp
@@ -7,19 +7,21 @@ int main() {
     for (int i = 0; i < 5; i++) {
         Q.push(i);
     }
+
+    // Q.push(8);
     for (int i = 0; i < 5; i++) {
         std::cout << Q.get_front() << std::endl;
     }
     for (int i = 0; i < 3; i++) {
         Q.push(i);
     }
-    std::cout << Q.get_front() << std::endl;
-    std::cout << Q.get_front() << std::endl;
-    std::cout << Q.get_front() << std::endl;
+    // std::cout << Q.get_front() << std::endl;
+    // std::cout << Q.get_front() << std::endl;
+    // std::cout << Q.get_front() << std::endl;
     // std::cout << Q.get_front() << std::endl;
 
 
-    Q.push(9);
+    // Q.push(9);
     std::cout << "Head: " << Q.get_head() << std::endl;
     std::cout << "Tail: " << Q.get_tail() << std::endl;
 
